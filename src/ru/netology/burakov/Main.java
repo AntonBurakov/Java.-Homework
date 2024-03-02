5a111416e36344ecfcb24d7d39eee9c7f3fd5701
antonburakov <anton.burakovv@gmail.com>
2024-03-03 02:41:51 +0600

    Homework 5

--- src/ru/netology/burakov/Main.java
+++ src/ru/netology/burakov/Main.java
@@ -4,6 +4,11 @@ import java.util.Date;
 import java.util.Scanner;
 import java.text.SimpleDateFormat;
 
+import model.Customer;
+import model.Operation;
+import repository.CustomerRepository;
+import repository.OperationRepository;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -73,7 +78,7 @@ public class Main {
         System.out.println("Номер счета получателя: " + accountNumber);
         System.out.println("Код валюты: " + currencyCode);
         System.out.println("Размер перевода: " + amount);
-        */
+
 
         Scanner scanner3 = new Scanner(System.in);
         double[] transactions = new double[5];
@@ -119,6 +124,83 @@ public class Main {
         for (int i = 0; i < transactions.length; i++) {
             if ((dates[i].after(dateStart)) && (dates[i].before(dateEnd))) {
                 System.out.println(transactions[i] + " за дату " + dates[i]);
+            }
+        }
+    }
+    */
+        // ДЗ5
+        CustomerRepository customerRepository = new CustomerRepository();
+        OperationRepository operationRepository = new OperationRepository();
+        Customer customer;
+        Operation operation;
+
+
+        Scanner scanner = new Scanner(System.in);
+
+        boolean b = true;
+
+        while (b) {
+
+            System.out.println("""
+                Добрый день, какую из нижеперечисленных операций желаете выполнить?
+                1. Добавить получателя
+                2. Выполнить перевод
+                3. Вывести список операций по получателю
+                4. Вывести полный список получателей
+                5. Вывести полный список операций
+                6. Заверщение работы
+                """);
+
+            int command = scanner.nextInt();
+
+            switch (command) {
+                case 1:
+                    System.out.println("Введите Имя получателя: ");
+                    String firstName = scanner.next();
+                    System.out.println("Введите Фамилию получателя: ");
+                    String secondName = scanner.next();
+                    System.out.println("Введите возраст получателя: ");
+                    int age = scanner.nextInt();
+                    customer = new Customer(firstName,secondName,age);
+                    customerRepository.addCustomer(customer);
+                    break;
+                case 2:
+                    System.out.println("Введите сумму: ");
+                    double amount = scanner.nextDouble();
+                    System.out.println("Введите дату: ");
+                    int date = scanner.nextInt();
+                    System.out.println("Введите имя получателя");
+                    String name = scanner.next();
+                    int customerId = customerRepository.findCustomerRepo(name);
+                    if(customerId == 404){
+                        System.out.println("Повторите попытку.");
+                    } else {
+                        operation = new Operation(amount, date);
+                        operationRepository.addOperation(operation, customerId);
+                    }
+                    break;
+                case 3:
+                    System.out.println("Введите имя получателя: ");
+                    String nameCustomer = scanner.next();
+                    int nameId = customerRepository.findCustomerRepo(nameCustomer);
+                    Operation[] operationsCustomer = OperationRepository.getOperations(nameId);
+                    operationRepository.printOperation(operationsCustomer);
+                    break;
+                case 4:
+                    System.out.println("Полный список получателей: ");
+                    customerRepository.print();
+                    break;
+                case 5:
+                    System.out.println("Полный список операций ");
+                    operationRepository.print();
+                    break;
+                case 6:
+                    System.out.println("Это было супер!");
+                    b = false;
+                    break;
+                default:
+                    System.out.println("Такой команды не существует");
+                    break;
             }
         }
     }
---
+++ src/service/ConsolePrintable.java
@@ -0,0 +1,5 @@
+package service;
+
+public interface ConsolePrintable {
+    void print();
+}
